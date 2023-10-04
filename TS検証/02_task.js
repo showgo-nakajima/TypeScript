f6833ed07ed9a61d5cad3d6c0936f7fd3ed61509
showgo-nakajima <ebatech341@gmail.com>
2023-10-04 15:07:48 +0900

    02_taskに削除機能を実装しました。

--- TS検証/02_task.js
+++ TS検証/02_task.js
@@ -7,9 +7,11 @@ var dateInput = document.getElementById("date");
 var descriptionInput = document.getElementById("description");
 var scheduleList = document.getElementById("scheduleList");
 var errorDisplay = document.getElementById("errorDisplay");
+var completionMessage = document.getElementById("completionMessage");
+var messageDisplayTime = 5000; // メッセージを表示する時間（ミリ秒）
 // フォームの送信時の処理
 scheduleForm.addEventListener("submit", function (e) {
-    e.preventDefault(); // フォームのデフォルト動作を無効化
+    e.preventDefault();
     var title = titleInput.value.trim();
     var dateString = dateInput.value;
     var description = descriptionInput.value.trim();
@@ -17,10 +19,6 @@ scheduleForm.addEventListener("submit", function (e) {
         var date = new Date(dateString);
         if (!isNaN(date.getTime())) {
             addSchedule(title, date, description);
-            titleInput.value = "";
-            dateInput.value = "";
-            descriptionInput.value = "";
-            errorDisplay.style.display = "none";
         }
         else {
             displayError("Invalid date format. Please use YYYY-MM-DD.");
@@ -29,6 +27,10 @@ scheduleForm.addEventListener("submit", function (e) {
     else {
         displayError("Title and date are required.");
     }
+    // 入力フォームをクリア
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
 });
 // スケジュールを追加する関数
 function addSchedule(title, date, description) {
@@ -37,18 +39,59 @@ function addSchedule(title, date, description) {
         title: title,
         date: date,
         description: description,
+        deleted: false,
     };
     schedules.push(newSchedule);
     displaySchedules();
 }
+// スケジュールを削除する関数
+function deleteSchedule(id) {
+    console.log("deleteSchedule called with id: ".concat(id));
+    var deletedIndex = -1;
+    for (var i = 0; i < schedules.length; i++) {
+        if (schedules[i].id === id) {
+            deletedIndex = i;
+            break;
+        }
+    }
+    if (deletedIndex !== -1) {
+        var deletedSchedule = schedules.splice(deletedIndex, 1)[0]; // スケジュールを削除し、削除されたスケジュールを取得
+        // 削除メッセージを表示
+        completionMessage.textContent = "\"".concat(deletedSchedule.title, "\"\u306F\u5B8C\u4E86\u3057\u307E\u3057\u305F\u306D\uFF01");
+        completionMessage.style.display = "block";
+        // メッセージを非表示にするタイマーを設定
+        setTimeout(function () {
+            completionMessage.style.display = "none";
+        }, messageDisplayTime);
+        // コンソールに削除メッセージを表示
+        console.log("\"".concat(deletedSchedule.title, "\" \u304C\u524A\u9664\u3055\u308C\u307E\u3057\u305F"));
+    }
+    displaySchedules();
+}
 // スケジュールを表示する関数
 function displaySchedules() {
-    scheduleList.innerHTML = ""; // 一覧をクリア
+    scheduleList.innerHTML = "";
+    var _loop_1 = function (schedule) {
+        var listItem = document.createElement("li");
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "削除";
+        // 削除ボタンのクリックイベントリスナを追加
+        deleteButton.addEventListener("click", function () {
+            deleteSchedule(schedule.id);
+            // クリックされたボタンの親要素である <li> を非表示にする
+            listItem.style.display = "none";
+        });
+        listItem.innerHTML += "Title: ".concat(schedule.title, ", Date: ").concat(schedule.date.toLocaleDateString(), ", Description: ").concat(schedule.description);
+        if (schedule.deleted) {
+            listItem.style.textDecoration = "line-through";
+        }
+        // <li> をリストに追加
+        scheduleList.appendChild(listItem);
+        listItem.appendChild(deleteButton);
+    };
     for (var _i = 0, schedules_1 = schedules; _i < schedules_1.length; _i++) {
         var schedule = schedules_1[_i];
-        var listItem = document.createElement("li");
-        listItem.textContent = "Title: ".concat(schedule.title, ", Date: ").concat(schedule.date.toLocaleDateString(), ", Description: ").concat(schedule.description);
-        scheduleList.appendChild(listItem);
+        _loop_1(schedule);
     }
 }
 // エラーメッセージを表示する関数
--- TS検証/02_task.ts
+++ TS検証/02_task.ts
@@ -2,8 +2,9 @@
 interface Schedule {
   id: number;
   title: string;
-  date: Date; // 日付は Date オブジェクトとして保存
+  date: Date;
   description: string;
+  deleted?: boolean; // 削除状態を管理するプロパティ
 }
 
 // スケジュールの一覧
@@ -16,10 +17,12 @@ const dateInput = document.getElementById("date") as HTMLInputElement;
 const descriptionInput = document.getElementById("description") as HTMLInputElement;
 const scheduleList = document.getElementById("scheduleList") as HTMLUListElement;
 const errorDisplay = document.getElementById("errorDisplay") as HTMLDivElement;
+const completionMessage = document.getElementById("completionMessage") as HTMLDivElement;
+const messageDisplayTime = 5000; // メッセージを表示する時間（ミリ秒）
 
 // フォームの送信時の処理
-scheduleForm.addEventListener("submit", function (e) {
-  e.preventDefault(); // フォームのデフォルト動作を無効化
+scheduleForm.addEventListener("submit", function (e: Event) {
+  e.preventDefault();
 
   const title = titleInput.value.trim();
   const dateString = dateInput.value;
@@ -29,16 +32,17 @@ scheduleForm.addEventListener("submit", function (e) {
     const date = new Date(dateString);
     if (!isNaN(date.getTime())) {
       addSchedule(title, date, description);
-      titleInput.value = "";
-      dateInput.value = "";
-      descriptionInput.value = "";
-      errorDisplay.style.display = "none";
     } else {
       displayError("Invalid date format. Please use YYYY-MM-DD.");
     }
   } else {
     displayError("Title and date are required.");
   }
+
+  // 入力フォームをクリア
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
 });
 
 // スケジュールを追加する関数
@@ -48,19 +52,68 @@ function addSchedule(title: string, date: Date, description: string): void {
     title,
     date,
     description,
+    deleted: false,
   };
   schedules.push(newSchedule);
   displaySchedules();
 }
 
+// スケジュールを削除する関数
+function deleteSchedule(id: number): void {
+  console.log(`deleteSchedule called with id: ${id}`);
+
+  let deletedIndex = -1;
+  for (let i = 0; i < schedules.length; i++) {
+    if (schedules[i].id === id) {
+      deletedIndex = i;
+      break;
+    }
+  }
+
+  if (deletedIndex !== -1) {
+    const deletedSchedule = schedules.splice(deletedIndex, 1)[0]; // スケジュールを削除し、削除されたスケジュールを取得
+
+    // 削除メッセージを表示
+    completionMessage.textContent = `"${deletedSchedule.title}"は完了しましたね！`;
+    completionMessage.style.display = "block";
+
+    // メッセージを非表示にするタイマーを設定
+    setTimeout(() => {
+      completionMessage.style.display = "none";
+    }, messageDisplayTime);
+
+    // コンソールに削除メッセージを表示
+    console.log(`"${deletedSchedule.title}" が削除されました`);
+  }
+
+  displaySchedules();
+}
+
 // スケジュールを表示する関数
 function displaySchedules(): void {
-  scheduleList.innerHTML = ""; // 一覧をクリア
+  scheduleList.innerHTML = "";
 
   for (const schedule of schedules) {
     const listItem = document.createElement("li");
-    listItem.textContent = `Title: ${schedule.title}, Date: ${schedule.date.toLocaleDateString()}, Description: ${schedule.description}`;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "完了";
+
+    // 削除ボタンのクリックイベントリスナを追加
+    deleteButton.addEventListener("click", () => {
+      deleteSchedule(schedule.id);
+      // クリックされたボタンの親要素である <li> を非表示にする
+      listItem.style.display = "none";
+    });
+
+    listItem.innerHTML += `Title: ${schedule.title}, Date: ${schedule.date.toLocaleDateString()}, Description: ${schedule.description}`;
+
+    if (schedule.deleted) {
+      listItem.style.textDecoration = "line-through";
+    }
+
+    // <li> をリストに追加
     scheduleList.appendChild(listItem);
+    listItem.appendChild(deleteButton);
   }
 }
 
